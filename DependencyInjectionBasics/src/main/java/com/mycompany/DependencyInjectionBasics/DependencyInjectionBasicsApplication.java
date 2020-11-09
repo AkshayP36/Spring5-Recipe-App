bc606fc59e6f5b0af4487868f39555782c76715d
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-11-09 22:52:16 +0530

    Initial commit of skeleton project

---
+++ DependencyInjectionBasics/src/main/java/com/mycompany/DependencyInjectionBasics/DependencyInjectionBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.DependencyInjectionBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionBasicsApplication.class, args);
+	}
+
+}
---
+++ DependencyInjectionBasics/src/test/java/com/mycompany/DependencyInjectionBasics/DependencyInjectionBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.DependencyInjectionBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
